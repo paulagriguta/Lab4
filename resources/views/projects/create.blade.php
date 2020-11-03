bd24038deef3b6ed71490a6b090eb48842dccd36
Paula Augustin <griguta.paula@gmail.com>
2020-11-03 10:50:37 +0200

    first commit

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Adaugă proiect</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('projects.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> Ai niște erori.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('projects.store') }}" method="POST" >
+        @csrf
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nume:</strong>
+                    <input type="text" name="name" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Introducere:</strong>
+                    <textarea class="form-control" style="height:50px" name="introduction"
+                        placeholder="Introduction"></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Locatie:</strong>
+                    <input type="text" name="location" class="form-control" placeholder="Location">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Cost:</strong>
+                    <input type="number" name="cost" class="form-control" placeholder="Cost">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Trimite</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
